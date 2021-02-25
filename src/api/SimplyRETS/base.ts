16ff23303b653c04ca4651a7448d960c7e0f39b8
Udayan Shevade <udayan.shevade@gmail.com>
2021-02-24 23:09:51 -0800

    update: use Authorization header in api

--- src/api/SimplyRETS/base.ts
+++ src/api/SimplyRETS/base.ts
@@ -1,9 +1,11 @@
 import axios, { AxiosInstance } from 'axios';
 
+const auth = btoa("simplyrets:simplyrets");
 const baseURL = 'https://api.simplyrets.com';
 
 const simplyRetsInstance: AxiosInstance = axios.create({
   baseURL,
+  headers: { Authorization: `Basic ${auth}` }
 });
 
 export default simplyRetsInstance;
