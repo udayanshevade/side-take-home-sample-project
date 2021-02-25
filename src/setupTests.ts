6774e0917b1d80eab44a4175936d76cb8d398724
Udayan Shevade <udayan.shevade@gmail.com>
2021-02-24 19:33:12 -0800

    tests: set up global mock storage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,35 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+/**
+ * Putting this global mock for the local storage here
+ * If this gets too cluttered, the file structure for test setup can be re-evaluated
+ */
+class LocalStorageMock {
+  store: { [key: string]: string };
+  length: number = 0;
+  key: (index: number) => string | null = () => null;
+
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: any) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
