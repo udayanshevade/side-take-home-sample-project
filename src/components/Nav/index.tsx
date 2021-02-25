ef3c4400089f77170e30542032c893e1cde44875
Udayan Shevade <udayan.shevade@gmail.com>
2021-02-24 20:28:09 -0800

    styles: initial Nav styles

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import routes, { NavItem } from './routes';
+import './index.scss';
 
 const Nav = ({ items = routes }: { items?: NavItem[] }) => (
   <nav className="nav">
