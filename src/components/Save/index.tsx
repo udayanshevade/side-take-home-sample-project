82a1d16309a20bc4fb8bd21f2e9531c28a95749e
Udayan Shevade <udayan.shevade@gmail.com>
2021-02-24 22:36:39 -0800

    styles: refine view details

--- src/components/Save/index.tsx
+++ src/components/Save/index.tsx
@@ -1,35 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React from 'react';
 import IconEmpty from '../Icons/LikeEmpty';
 import IconFilled from '../Icons/LikeFilled';
-import * as storageApi from '../../api/save';
-
-const useSavedListings = () => {
-  const [savedListings, setSavedListings] = useState<{ [id: string]: true }>({});
-
-  // store the listings (local storage for now)
-  // using a callback so that persistListings
-  const persistListings = useCallback(
-    (id: number, toBeSaved: boolean) => {
-      let toSave = { ...savedListings };
-      if (toBeSaved) {
-        toSave[id] = true;
-      } else {
-        const { [id]: _, ...restListings } = savedListings;
-        toSave = restListings;
-      }
-      storageApi.storeSavedListings(toSave);
-      setSavedListings(toSave);
-    },
-    [savedListings, setSavedListings],
-  );
-
-  useEffect(() => {
-    const saved = storageApi.getSavedListings();
-    if (saved) setSavedListings(saved);
-  }, []);
-
-  return [savedListings, persistListings] as const;
-};
+import useSavedListings from './useSavedListings';
 
 const Save = ({ mlsId }: { mlsId: number }) => {
   const [saved, persistListings] = useSavedListings();
