8e1efdc6e996a9473645f9660d7da2964e31bef0
Udayan Shevade <udayan.shevade@gmail.com>
2021-02-24 22:10:23 -0800

    storagApi: add simple handlers for saving listing

---
+++ src/api/save/index.ts
@@ -0,0 +1,33 @@
+const tag = 'save-listings-api';
+
+/**
+ * Local placeholder for what might eventually be a REST API call
+ */
+export const getSavedListings = (): { [id: string]: true } | null => {
+  try {
+    const saves = window.localStorage.getItem('saves');
+    if (saves) {
+      return JSON.parse(saves);
+    } else {
+      return {};
+    }
+  } catch (err) {
+    console.error(tag, `Error: localStorage.getItem('saves'): ${err.message}`);
+    return null;
+  }
+};
+
+/**
+ * The returned boolean indicates whether the "call" was successful.
+ * In the future, this might be a PATCH that updates the saved status via the backend.
+ * The caller can use the return to judge how to handle the subsequent scenarios.
+ */
+export const storeSavedListings = (ids: { [id: string]: boolean }): boolean => {
+  try {
+    window.localStorage.setItem('saves', JSON.stringify(ids));
+    return true;
+  } catch (err) {
+    console.error(tag, `Error: localStorage.setItem('saves'): ${err.message}`);
+    return false;
+  }
+};
